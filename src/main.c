c691af7e6e4e653d58e740e8bccd6f5b9c039a87
Caio Henrique Dos Santos Carvalho <105748367+Caio-HS@users.noreply.github.com>
2026-03-27 23:45:53 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,12 +2,14 @@
 
 #include "get_params.h"
 #include "create_ann.h"
+#include "data.h"
 
 int main(int argc, char * argv[])
 {
     int error_code = 0;
 
     struct fann * ann = NULL;
+    struct fann_train_data * data = NULL;
     PARAMETERS params = { .neurons_by_layer = NULL, .activation_by_layer = NULL };
 
     error_code = get_parameter((unsigned int)argc, (const char * const *)argv, &params);
@@ -16,11 +18,15 @@ int main(int argc, char * argv[])
     error_code = create_configured_ann(&ann, params); 
     if(error_code != 0) {goto cleanup;}
 
+    error_code = get_train_data(&data, DATA_FILENAME);
+    if(error_code != 0) {goto cleanup;}
+
     //Essa secao deve virar uma funcao propria
 cleanup:
     if (params.neurons_by_layer) free(params.neurons_by_layer);
     if (params.activation_by_layer) free(params.activation_by_layer);
     if (ann) fann_destroy(ann);
+    if(data) fann_destroy_train(data);
 
     return error_code;
 }
