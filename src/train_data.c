4d3e3447e9d299943d5f03e9f6e1187ad77f92c0
Caio Henrique Dos Santos Carvalho <105748367+Caio-HS@users.noreply.github.com>
2026-03-27 23:37:54 -0300

    Update train_data.c

--- src/train_data.c
+++ src/train_data.c
@@ -1,8 +1,13 @@
 #include <fann.h>
+#include <errno.h>
 
 #include "train_data.h"
 
-int train_data (struct fann_train_data * restrict const data, const char * restrict const filename)
+int train_data(struct fann_train_data * restrict const data, const char * restrict const filename)
 {
+    if(data == NULL) { return EINVAL; }
+
+    *data = fann_read_train_from_file(filename);
+    
     return 0;
 }
