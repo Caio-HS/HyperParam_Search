3d3ed2e21fa1926432971551f0681399fde6ec5e
Caio Henrique Dos Santos Carvalho <105748367+Caio-HS@users.noreply.github.com>
2026-04-01 14:44:58 -0300

    Update result.h

--- include/result.h
+++ include/result.h
@@ -27,7 +27,7 @@ typedef struct results RESULTS;
 
 int instrument_ann(struct fann * restrict const ann, CONTEXT ** restrict const context);
 int set_train_results(struct fann * restrict const ann, uint64_t time, RESULTS * restrict const results);
-int set_test_results(struct fann * restrict const ann, uint64_t time, RESULTS * restrict const results)
+int set_test_results(struct fann * restrict const ann, uint64_t time, RESULTS * restrict const results);
 int send_results(RESULTS * restrict const results, int * restrict const result_vector);
 
 #endif
