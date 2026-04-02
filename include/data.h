cc26611e83cdefff122539c077ca7510f0ab5b23
Caio Henrique Dos Santos Carvalho <105748367+Caio-HS@users.noreply.github.com>
2026-04-02 15:14:00 -0300

    Update data.h

--- include/data.h
+++ include/data.h
@@ -4,6 +4,9 @@
 #include <fann.h>
 
 #define DATA_FILENAME "./data/energy_efficiency.data"
+#define DATA_USED_IN_TRAIN 0.750f
+#define MIN_SCALE -1.0f
+#define MAX_SCALE 1.0f
 
 int get_train_data(struct fann_train_data ** restrict const data, const char * restrict const filename);
 int get_test_data(struct fann_train_data ** restrict const data, const char * restrict const filename);
