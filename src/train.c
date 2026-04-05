82de363a3da48644617ae00ae9cfcd87ec31acb6
Caio Henrique Dos Santos Carvalho <105748367+Caio-HS@users.noreply.github.com>
2026-04-05 03:51:37 -0300

    Update train.c

--- src/train.c
+++ src/train.c
@@ -7,15 +7,9 @@
 
 #define NO_REPORTS 0
 
-int train_network(struct fann * restrict const ann, struct fann_train_data * restrict const data, RESULTS * restrict const results)
+int train_network(struct fann * restrict const ann, struct fann_train_data * restrict const data)
 {
-    clock_t inicio = clock();
-
     fann_train_on_data(ann, data, MAX_EPOCHS, NO_REPORTS, TARGET_ERROR);
-
-    clock_t fim = clock();
-
-    results->train_time = (uint64_t)(fim - inicio);
     
     return 0;
 }
