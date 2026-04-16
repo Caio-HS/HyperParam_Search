978f025064b6660a53541034dfaef5fffd22ddde
Caio Henrique Dos Santos Carvalho <105748367+Caio-HS@users.noreply.github.com>
2026-04-16 10:05:46 -0300

    Add hash header file with set_parameters_hash function

---
+++ include/hash.h
@@ -0,0 +1,9 @@
+#ifndef HASH
+#define HASH
+
+#include "get_params.h"
+#include "result.h"
+
+int set_parameters_hash(const PARAMETERS * restrict const params, RESULTS * restrict const results);
+
+#endif
