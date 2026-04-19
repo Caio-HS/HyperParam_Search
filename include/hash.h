31537a000b65c318c8f2ae90c2f58ee468ca4057
Caio Henrique Dos Santos Carvalho <105748367+Caio-HS@users.noreply.github.com>
2026-04-18 21:05:49 -0300

    Add HASHS_SEED definition in hash.h
    Define a constant for hash seed in hash.h

--- include/hash.h
+++ include/hash.h
@@ -4,6 +4,8 @@
 #include "get_params.h"
 #include "result.h"
 
+#define HASHS_SEED 0
+
 int set_parameters_hash(const PARAMETERS * restrict const params, RESULTS * restrict const results);
 
 #endif
