ff3fcfcab71afb46d86f589fa740821ffcf219b3
Caio Henrique Dos Santos Carvalho <105748367+Caio-HS@users.noreply.github.com>
2026-04-21 17:00:59 -0300

    Add parameters generator for network settings
    Initialize parameters for network configuration.

---
+++ orchestrator/parameters_generator.py
@@ -0,0 +1,20 @@
+import parameters as params
+
+len_network_type = len(params.network_type)
+len_train_algorithm = len(params.train_algorithm)
+len_error_function = len(params.error_function)
+len_density = len(params.density)
+len_num_layers = len(params.num_layers)
+len_neuron_numbers = len(params.neuron_numbers)
+len_activation_function = len(params.activation_function)
+
+current_network_type = 0
+current_train_algorithm = 0
+current_error_function = 0
+current_density = 0
+current_num_layers = 0
+current_neuron_numbers = 0
+current_activation_function = 0
+
+def next_param():
+  for
