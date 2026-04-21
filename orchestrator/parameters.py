7e3f16049bf2bcaf34d7e8924462bccd2ad975db
Caio Henrique Dos Santos Carvalho <105748367+Caio-HS@users.noreply.github.com>
2026-04-21 16:34:04 -0300

    Create parameters.py

---
+++ orchestrator/parameters.py
@@ -0,0 +1,6 @@
+num_layers = [ 4, 6, 10 ] # Potencias de 2 de 2^1 a 2^3  + 2, Camada de entrada + 2^n camadas ocultas + Camada de saida
+density = [ 0.01, 0.10, 0.50, 0.90, 1.0 ] # Quantos % das conexoes entre neuronios vao existir, 1.0 significa 100%, 0.01 significa 1%
+network_type = [ 0, 1 ] 
+error_function = [ 0, 1 ] 
+train_algorithm = [ 0, 1, 2 ]
+activation_function = [ 0, 1, 3, 7, 14 ]
