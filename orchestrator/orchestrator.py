1d36818d59a141e9f358a72e3b15e11d76db3322
Caio Henrique Dos Santos Carvalho <105748367+Caio-HS@users.noreply.github.com>
2026-06-04 21:15:16 -0300

    Orchestrator oversimplification
    Como eu estava me sentindo travado pela complexidade da implementacao do orquestrador, eu decidi, momentaneamente, reduzir ele a monotarefa, sem persistencia de progresso e tudo mais que ele requer.

--- orchestrator/orchestrator.py
+++ orchestrator/orchestrator.py
@@ -1,45 +1,28 @@
-import time
 import subprocess as proc
 import struct
-from typing import NamedTuple
 
 import database as db
 import parameters_generator as params_gen
 
-active_process = []
-MAX_PROCESS = 2
 WORKER_NAME = "../worker"
-RETURN_FORMAT = "QQQQQfQfQQ"
-RETURN_SIZE = 80
-
-class Execution(NamedTuple):
-    parameters: string
-    p: proc.Popen
+RETURN_FORMAT = "QQQQQfQQfQ"
+RETURN_SIZE = 72
 
 conn = db.setup()
 
 while true:
 
-    while (len(active_process) < MAX_PROCESS):
-        parameters = params_gen.next_params()
-        cmd = WORKER_NAME + parameters
-        p = proc.Popen(cmd, stdout=subprocess.PIPE)
-        work = Execution(parameters, p)
-        active_process.append(work)
+    parameters = params_gen.next_params()
+    cmd = WORKER_NAME + parameters
+    p = proc.Popen(cmd, stdout=subprocess.PIPE)
 
-    for work in active_process:
-        returned = work.p.poll()
+    output, errors_outputed = p.communicate()
 
-        if returned is None:
-            continue
-            
-        if returned is not 0:
-            print(f"Worker pid: {work.p.pid}, parameters: {work.parameters}, returned: {returned}")
-
-        active_process.remove(work)
-        raw_result = work.p.stdout.read()
-        res = struct.unpack(RETURN_FORMAT, raw_result[:RETURN_SIZE])
-        db.add_record(conn, res, work.parameters)
-
+    returned = p.returncode
     
-    time.sleep(1)
+    if returned is not 0:
+        print(f"Worker pid: {work.p.pid}, parameters: {work.parameters}, returned: {returned}")
+
+    raw_result = p.stdout.read()
+    res = struct.unpack(RETURN_FORMAT, raw_result[:RETURN_SIZE])
+    db.add_record(conn, res, parameters)
